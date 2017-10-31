aa29e73177c043e85fae853b8d2579101d5c3c7b
Guillaume Jacquart <guillaume.jacquart@live.fr>
2017-10-31 17:21:42 +0000

    Add update and delete of app.

--- public/apps.js
+++ public/apps.js
@@ -20,20 +20,38 @@ var app = new Vue({
                 }
             });
         },
-        addApp: function() {
-            fetch("../apps", {
-                method: "POST",
+        saveApp: function() {
+            var url = this.newApp.isUpdate ? ('../apps/' + this.newApp.name) : '../apps';
+            var method = this.newApp.isUpdate ? 'PUT' : 'POST'
+            fetch(url, {
+                method: method,
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: this.newApp
             }).then(function(res) {
                 if (res.status == 200) {
-                    alert("Perfect! Your settings are saved.");
+                    alert("App has been saved !");
                 }
             }, function(e) {
                 alert("Error submitting form!");
             });
-        }
+        },
+        deleteApp: function(name) {
+            fetch("../apps/" + name, {
+                method: "DELETE"
+            }).then(function(res) {
+                if (res.status == 200) {
+                    alert("App has been deleted !");
+                }
+            }, function(e) {
+                alert("Error submitting form!");
+            });
+        },
+        updateForm: function(app) {
+            this.newApp = app;
+            this.newApp.isUpdate = true;
+            $('#add-app').modal('show');
+        },
     }
 })
